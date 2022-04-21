5d387d6251e1f7cd0445ad2310d356cf8190590d
David Inga <david.inga@vizzuality.com>
2022-04-21 10:19:13 +0200

    project mock updated

--- src/pages/api/projects/keo-seima.ts
+++ src/pages/api/projects/keo-seima.ts
@@ -1,6 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import MockData from 'data/keo-seima-mock.json';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import MockData from 'data/keo-seima-mock.json'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(MockData);
-};
+const mockKeoSeima = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json(MockData)
+}
+
+export default mockKeoSeima
