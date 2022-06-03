faf53cd2d8848d086af3d6c9675a5ba354c8fb87
anamontiaga <amontiaga@gmail.com>
2022-06-03 11:10:08 +0200

    sketch for desktop navigation menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,6 +10,7 @@ import { Text } from 'components/Text'
 import Link from 'next/link'
 import { Icon } from 'components/Icon'
 import { NavMenuDialog } from 'components/NavMenuDialog/NavMenuDialog'
+import { useFetchProjectsList } from 'hooks/fetchProjectsList'
 
 type HeaderOwnProps = ComponentProps<typeof HeaderRoot>
 
@@ -17,6 +18,9 @@ type HeaderProps = HeaderOwnProps
 
 export const Header = (props: HeaderProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [submenu, setSubmenu] = useState([false, false])
+
+  const { data: projectsList } = useFetchProjectsList()
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
@@ -33,17 +37,108 @@ export const Header = (props: HeaderProps) => {
           </a>
 
           <Nav>
-            <Link href="/projects" passHref>
-              <Text as="a" color="neutral-white">
-                PROJECTS
+            <div
+              onMouseEnter={() => setSubmenu([true, false])}
+              onMouseLeave={() => setSubmenu([false, false])}
+              style={{ position: 'relative', height: '30px' }}
+            >
+              <Text as="a" color="neutral-white" weight="bold">
+                Projects
               </Text>
-            </Link>
+              {submenu[0] && (
+                <div
+                  onMouseEnter={() => setSubmenu([true, false])}
+                  onMouseLeave={() => setSubmenu([false, false])}
+                  style={{
+                    background: 'white',
+                    position: 'absolute',
+                    padding: '10px',
+                    width: '150px',
+                    borderRadius: '4px',
+                    top: '30px',
+                    left: '-10px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  {projectsList.map((p) => (
+                    <div key={p.id} style={{ marginBottom: '8px' }}>
+                      <Link href={`/projects/${p.id}`} passHref>
+                        <Text as="a" color="primary-jasper-500" weight="bold">
+                          {p.title}
+                        </Text>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+
             <a href="https://everland.earth/contact/" rel="noreferrer" target="_blank">
-              <Text color="neutral-white">CONTACT US</Text>
+              <Text color="neutral-white" weight="bold">
+                Contact Us
+              </Text>
             </a>
-            <a href="https://everland.earth/who-we-are" rel="noreferrer" target="_blank">
-              <Text color="neutral-white">ABOUT US</Text>
-            </a>
+
+            <div
+              onMouseEnter={() => setSubmenu([false, true])}
+              onMouseLeave={() => setSubmenu([false, false])}
+              style={{ position: 'relative', height: '30px' }}
+            >
+              <Text color="neutral-white" weight="bold">
+                About Us
+              </Text>
+              {submenu[1] && (
+                <div
+                  onMouseEnter={() => setSubmenu([false, true])}
+                  onMouseLeave={() => setSubmenu([false, false])}
+                  style={{
+                    background: 'white',
+                    position: 'absolute',
+                    padding: '10px',
+                    width: '150px',
+                    borderRadius: '4px',
+                    top: '30px',
+                    left: '-10px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <a
+                    style={{ marginBottom: '8px' }}
+                    href="https://everland.earth/who-we-are"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <Text as="a" color="primary-jasper-500" weight="bold">
+                      Who We Are
+                    </Text>
+                  </a>
+
+                  <a
+                    style={{ marginBottom: '8px' }}
+                    href="https://everland.earth/what-we-do"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <Text as="a" color="primary-jasper-500" weight="bold">
+                      What We Do
+                    </Text>
+                  </a>
+
+                  <a
+                    style={{ marginBottom: '8px' }}
+                    href="https://everland.earth/our-offer"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <Text as="a" color="primary-jasper-500" weight="bold">
+                      Our Offer
+                    </Text>
+                  </a>
+                </div>
+              )}
+            </div>
           </Nav>
 
           <Menu onClick={toggleMenu}>
